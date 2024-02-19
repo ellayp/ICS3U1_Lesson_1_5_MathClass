19650d6962ddd5c527334d240cb6624183f31111
ellas <ella.yap26@ycdsbk12.ca>
2024-02-19 17:57:39 -0500

    Updated Program1.java

--- Program1.java
+++ Program1.java
@@ -1,13 +1,27 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Lets you enter the two sides of a right angled triangle, and then print the hypotenuse.
+  * @author: E.Yap
   */
   
   public void run() {
-    // your code goes here
-    
-    
+    // Create variables for sideA, sideB, hypotenuse
+    double dblSideA;
+    double dblSideB;
+
+    double dblHypSqrd;
+    double dblHypotenuse;
+
+    // Get values for sideA, sideB
+    dblSideA = readDouble("Enter the length of side A: ");
+    dblSideB = readDouble("Enter the length of side B: ");
+
+    // Compute hypotenuse**2 = sideA**2 + sideB**2
+    dblHypSqrd = Math.pow(dblSideA,2) + Math.pow(dblSideB,2);
+    dblHypotenuse = Math.sqrt(dblHypSqrd);
+
+    // Output hypotenuse
+    System.out.println("The hypotenuse of the right triangle is " + dblHypotenuse);
   }
 }
